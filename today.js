32636370ed10933eba5f330d2a31bb8c7add1d42
Melanie E Magdalena <archgirl@users.noreply.github.com>
2018-03-04 17:29:05 -0700

    remove extra ;

--- today.js
+++ today.js
@@ -6,7 +6,7 @@ function calendar(){
   setText('calendar-day', day[d.getDay()]);
   setText('calendar-date', d.getDate());
   setText('calendar-month-year', month[d.getMonth()] + ' ' + (1900 + d.getYear()));
-};
+}
 
 // set vale of <p> tags
 function setText(id, val){
@@ -14,7 +14,7 @@ function setText(id, val){
     val = '0' + val; // add leading 0 if val < 10
   }
   document.getElementById(id).innerHTML = val;
-};
+}
 
 // call calendar() when page loads
 window.onload = calendar;
